798711e08a743e4720f8929e7c761afdd4df0a67
Jacob Zamir <jakezamir@gmail.com>
2026-06-22 21:27:49 -0700

    Calculates the JDN for different dates and calculates age and number of days alive for those dates.

--- Main.java
+++ Main.java
@@ -9,13 +9,31 @@ public class Main
 		/***** DECLARATION SECTION *****/
 
 		/***** INITIALIZATION SECTION *****/
-		
+		int todaysDateMonth = 6;
+        int todaysDateDay = 22;
+        int todaysDateYear = 2026;
+        int birthdayMonth = 2;
+        int birthdayDay = 23;
+        int birthdayYear = 2009;
+
 		/***** INPUT SECTION *****/
 		// N/A (no input for this lab)
 		
 		/***** CALCULATION & PROCESSING SECTION *****/
+        int aForToday = (14 - todaysDateMonth) / 12;
+        int yForToday = todaysDateYear + 4800 - aForToday;
+        int mForToday = todaysDateMonth + (12 * aForToday) - 3;
+        int jdnForToday = todaysDateDay + ((153 * mForToday + 2) / 5) + (365 * yForToday) + (yForToday / 4) - (yForToday / 100) + (yForToday / 400) - 32045;
 
-		
+        int aForBirthday = (14 - birthdayMonth) / 12;
+        int yForBirthday = birthdayYear + 4800 - aForBirthday;
+        int mForBirthday = birthdayMonth + (12 * aForBirthday) - 3;
+        int jdnForBirthday = birthdayDay + ((153 * mForBirthday + 2) / 5) + (365 * yForBirthday) + (yForBirthday / 4) - (yForBirthday / 100) + (yForBirthday / 400) - 32045;
+        int differenceInDays = jdnForToday  - jdnForBirthday;
+        int numberofYears = differenceInDays / 365;
 		/***** OUTPUT SECTION *****/
+        System.out.println("Julian Day Number for today's date, " + todaysDateMonth + "/" + todaysDateDay + "/" + todaysDateYear + " is " + jdnForToday);
+        System.out.println("Julian Day Number for birthday, " + birthdayMonth + "/" + birthdayDay + "/" + birthdayYear + ", is " + jdnForBirthday);
+        System.out.println("The difference in days is " + differenceInDays + ", which makes you approximately " + numberofYears + " years old!");
   }
 }
